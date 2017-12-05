72afecb510a3f28e9719c0efef3782f604eba696
Andrew Kim <andrew@Andrews-MacBook-Pro.local>
2017-12-04 16:53:35 -0800

    added increase and decrease button on cart page

--- js/app.js
+++ js/app.js
@@ -53,11 +53,47 @@ new Vue({
     },
     methods: {
         addProductToCart: function(product) {
-            this.cart.items.push({
-                product: product,
-                quantity: 1
-            });
+            var cartItem = this.getCartItem(product);
+
+            if(cartItem != null) {
+                cartItem.quantity++;
+            }else {
+                this.cart.items.push({
+                    product: product,
+                    quantity: 1
+                });
+            }
+
             product.inStock--;
+        },
+
+        getCartItem: function(product) {
+            for (var i=0; i < this.cart.items.length; i++) {
+                if(this.cart.items[i].product.id === product.id) {
+                    return this.cart.items[i];
+                }
+            }
+            
+            return null;
+        },
+        increaseQuantity: function(cartItem) {
+            cartItem.product.inStock--;
+            cartItem.quantity++;
+        },
+        decreaseQuantity: function(cartItem) {
+            cartItem.product.inStock++;
+            cartItem.quantity--;
+
+            if(cartItem.quantity == 0) {
+                this.removeItemFromCart(cartItem);
+            }
+        },
+        removeItemFromCart: function(cartItem) {
+            var index = this.cart.items.indexOf(cartItem);
+
+            if(index !== -1) {
+                this.cart.items.splice(index, 1);
+            }
         }
     },
     computed: {
